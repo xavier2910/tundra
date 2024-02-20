49e6c4d0ef1ca224a51a7380938d3e9101ba2dca
xavier2910 <x.distelzweig@gmail.com>
2024-02-20 12:30:57 -0600

    implemented basic command parser

--- commandproc.go
+++ commandproc.go
@@ -1,5 +1,7 @@
 package tundra
 
+import "strings"
+
 type CommandResultType int
 
 const (
@@ -8,9 +10,100 @@ const (
 	Death
 )
 
-type Command func(*World) (CommandResults, error)
+// The first parameter is a list of targets for the command (eg, examine CLOSET, put FISH on TABLE).
+// The second is for taking inventory, breaking the world, changing player pos, etc.
+type Command func([]*Object, *World) (CommandResults, error)
 
 type CommandResults struct {
 	Result CommandResultType
 	Msg    []string // unless Result is Expo, only Msg[0] is displayed
+}
+
+type CommandProcessor struct {
+	commandContext map[string]Command
+	objectContext  map[string]*Object
+}
+
+func NewCommandProcessor() *CommandProcessor {
+	return &CommandProcessor{}
+}
+
+func (cp *CommandProcessor) ClearCommandContext() {
+	cp.commandContext = make(map[string]Command, 0)
+}
+
+// Adds the given commands to the seach path of the
+// CommandProcessor, overwriting any existing
+// commands if a naming conflict arises.
+func (cp *CommandProcessor) AddCommandContext(additionalContext map[string]Command) {
+
+	for key, cmd := range additionalContext {
+		cp.commandContext[key] = cmd
+	}
+}
+
+// Adds the given commands to the seach path of the
+// CommandProcessor, skipping any new commands
+// if a naming conflict arises.
+func (cp *CommandProcessor) AppendCommandContext(additionalContext map[string]Command) {
+
+	for key, cmd := range additionalContext {
+
+		if cp.commandContext[key] == nil {
+			cp.commandContext[key] = cmd
+		}
+	}
+}
+
+func (cp *CommandProcessor) ClearObjectContext() {
+	cp.objectContext = make(map[string]*Object, 0)
+}
+
+// Adds the given commands to the seach path of the
+// CommandProcessor, overwriting any existing
+// commands if a naming conflict arises.
+func (cp *CommandProcessor) AddObjectContext(additionalContext map[string]*Object) {
+
+	for key, obj := range additionalContext {
+		cp.objectContext[key] = obj
+	}
+}
+
+// Adds the given commands to the seach path of the
+// CommandProcessor, skipping any new commands
+// if a naming conflict arises.
+func (cp *CommandProcessor) AppendObjectContext(additionalContext map[string]*Object) {
+
+	for key, obj := range additionalContext {
+
+		if cp.objectContext[key] == nil {
+			cp.objectContext[key] = obj
+		}
+	}
+}
+
+func (cp *CommandProcessor) Execute(cmd string, universe *World) (CommandResults, error) {
+
+	cmd, args := cp.preprocess(cmd)
+	parsedcmd, targets := cp.resolve(cmd, args)
+
+	return parsedcmd(targets, universe)
+}
+
+func (cp *CommandProcessor) preprocess(cmd string) (string, []string) {
+
+	split := strings.Fields(cmd)
+
+	return split[0], split[1:]
+}
+
+func (cp *CommandProcessor) resolve(cmd string, args []string) (Command, []*Object) {
+
+	arguments := make([]*Object, 2)
+
+	for i, arg := range args {
+		arguments[i] = cp.objectContext[arg]
+	}
+
+	return cp.commandContext[cmd], arguments
 }
