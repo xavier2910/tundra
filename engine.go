e39afbf4420d6b056e12d9911250299cddb22a16
xavier2910 <x.distelzweig@gmail.com>
2024-02-21 11:21:12 -0600

    added some documentation

--- engine.go
+++ engine.go
@@ -9,6 +9,14 @@ const (
 	West
 )
 
+// General behavior for a source of commands for the
+// command processor to look at.
+// Generally, commands should pe placed on the object
+// that is taking the action involved, not the object
+// the command is acting on. There are exceptions, such
+// as an unique command for an object that is not normally
+// available, e. g., a fly command for a spaceship,
+// or a shoot command for a laser.
 type HasCommands interface {
 	AddCommand(string, Command)
 	RemoveCommand(string)
